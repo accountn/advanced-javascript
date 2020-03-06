8729ce40811fa31fae537fff9f04964849345ac8
helal <helalimage12345@gmail.com>
2020-03-07 03:19:05 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,12 @@
+//
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const student = {name:"malka banu", phone:4564};
+
+console.log(student.phone);
